51d4ef061d0923c276973b6599dd580f83d20622
Лидия Пешкова <pesh.lida@yandex.ru>
2022-02-02 23:15:16 +0700

    дубль2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -26,7 +26,7 @@ class Post(models.Model):
         blank=True, null=True,
         on_delete=models.CASCADE
     )
-    
+
     def __str__(self):
         # выводим текст поста
         return self.text
