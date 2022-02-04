af8eba8a1a151b05e07236abfe6e2959ebe174b6
Лидия Пешкова <pesh.lida@yandex.ru>
2022-02-04 23:04:08 +0700

    дубль3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -15,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts =  group.posts.order_by('-pub_date')[:POSTS_LIMIT]
+    posts = group.posts.order_by('-pub_date')[:POSTS_LIMIT]
     context = {
         'group': group,
         'posts': posts,
